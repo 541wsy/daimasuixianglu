cbd83b42057d5fc85c10da738d223c98dda41cd6
541wsy <wsy4541@163.com>
2023-01-10 16:06:07 +0800

    update

--- 1.数组/移除元素/lc844.比较含退格的字符.py
+++ 1.数组/移除元素/lc844.比较含退格的字符.py
@@ -5,22 +5,31 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-
+        #方法1：双指针
         def get_string(s):
             slow = -1
-
-
             for fast in range(len(s)):
-
                 if s[fast] != '#':
                     if slow < 0:  # 字符串为空
                         continue
                     slow += 1
                     s[slow] = s[fast]
-
                 else:
                     slow -= 1
             return s[:slow + 1]
+        #方法2：栈
+        def get_string(s):
+            stack = []  # 建立一个空栈
+            s = list(s)
+            for element in s:
+                if element != "#":  # 如果没有碰到退格,进栈
+                    stack.append(element)
+                else:  # 如果碰到退格
+                    if len(stack) == 0:
+                        continue
+                    else:
+                        stack.pop()
+            return stack
 
         s_result = get_string(s)
         t_result = get_string(t)
