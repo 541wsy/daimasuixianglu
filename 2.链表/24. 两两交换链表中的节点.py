8a573fce2ee382384fb92c77b610d061bd9f1e8f
541wsy <wsy4541@163.com>
2023-02-01 15:11:27 +0800

    update

--- 2.链表/24. 两两交换链表中的节点.py
+++ 2.链表/24. 两两交换链表中的节点.py
@@ -9,24 +9,49 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
+
         # 初始化dummyhead
         dummyhead = ListNode()
         dummyhead.next = head
 
-        pred = dummyhead
-        curr = head
+        # 双指针法
+        # pred = dummyhead
+        # curr = head
 
-        while curr:
-            if not curr.next:  # 如果curr.next是空，即奇数情况
+        # while curr:
+        #     if not curr.next: #如果curr.next是空，即奇数情况
+        #         pred.next = curr
+        #         return dummyhead.next
+        #     pred.next = curr.next
+        #     newnode = pred.next.next
+        #     pred.next.next = curr
+
+        #     #更新指针
+        #     pred = curr
+        #     curr = newnode
+        # pred.next  = curr
+        # return dummyhead.next
+
+        # 递归
+        def inner_step(pred, curr):
+            if not curr:
                 pred.next = curr
                 return dummyhead.next
+            if not curr.next:
+                pred.next = curr
+                return dummyhead.next
+
+            # 执行操作
             pred.next = curr.next
             newnode = pred.next.next
             pred.next.next = curr
 
-            # 更新指针
-            pred = curr
-            curr = newnode
-        pred.next = curr
-        return dummyhead.next
+            # 更新迭代
+            return inner_step(curr, newnode)
+
+        return inner_step(dummyhead, head)
+
+
+
+
 
